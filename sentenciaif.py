230029aa3ee27d4579fce5fe27f8dfe2752518bc
Omar Mendoza <omaryahir@gmail.com>
2015-03-23 09:50:57 -0600

    CHG: elif on sentenciaif

--- sentenciaif.py
+++ sentenciaif.py
@@ -1,17 +1,15 @@
 # -*- encoding: utf8 -*-
 
-print("Comparador de años")
+print "Comparador de años"
 
-fecha1=int(input("¿En que año estamos?:"))
-fecha2=int(input("Escriba un año cualquiera:"))
+fecha1=int(raw_input("¿En que año estamos?:"))
+fecha2=int(raw_input("Escriba un año cualquiera:"))
 
 if fecha1 > fecha2:
-    print("Desde el año %s han pasado %s años" % (fecha2, (fecha1-fecha2))
-
-if fecha1 < fecha2:
-    print("Para llegar al año %s faltan %s años" % (fecha2, (fecha2-fecha1))
-
-if fecha1 == fecha2:
+    print("Desde el año %s han pasado %s años" % (fecha2, (fecha1-fecha2)))
+elif fecha1 < fecha2:
+    print("Para llegar al año %s faltan %s años" % (fecha2, (fecha2-fecha1)))
+else:
     print("¡Son el mismo año!")
 
 
