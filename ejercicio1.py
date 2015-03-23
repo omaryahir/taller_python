0b851c3055d798b7b1e1b4eb55b65733bc0f7106
Omar Mendoza <omaryahir@gmail.com>
2015-03-23 09:41:15 -0600

    Initial

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+sombrero = raw_input('Costo del sombrero:')
+abrigo = raw_input('costo del abrigo:')
+
+compra = int(sombrero) + int(abrigo)
+
+print "Me gaste $ " + str(compra) + " pesos; en un abrigo y un sombrero. "
+
---
+++ practica1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf8 -*-
+
+print "Cálculo de la media de dos numeros"
+num1 = float(raw_input("Escriba un número:"))
+num2 = float(raw_input("Escriba otro número:"))
+
+resultado = (num1 / 2) + (num2 / 2)
+
+print "La media aritmetica de " + str(num1) + " y " + str(num2) + " es " + str(resultado)
