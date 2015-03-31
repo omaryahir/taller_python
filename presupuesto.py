18a37c054f6492163717a93eecd2b23b17a5f019
Omar Mendoza <omaryahir@gmail.com>
2015-03-30 19:01:24 -0600

    ADD: finally

---
+++ presupuesto.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+import sys
+print sys.stdout.encoding
+
+
+print "=" * 60  + "\n"
+print "        GENERACION DE PRESUPUESTO \n" 
+print "=" * 60
+
+Empresa = raw_input('Empresa: ')
+Nomb1 = raw_input('Nombre del Cliente: ')
+Fechactual = raw_input('Fecha: ')
+Des = raw_input('Descripción del Servicio: ')
+Importe = raw_input('Importe Bruto: ')
+Fechacad = raw_input('Fecha de Caducidad: ')
